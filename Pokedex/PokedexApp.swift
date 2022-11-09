9d7521aef68fa8841d53a0a4894a203327c2190f
rutvi-s2 <rutvi01.shah@gmail.com>
2022-11-09 00:31:46 -0600

    Initial Commit

---
+++ Pokedex/PokedexApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokedexApp.swift
+//  Pokedex
+//
+//  Created by Rutvi Shah on 11/9/22.
+//
+
+import SwiftUI
+
+@main
+struct PokedexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
